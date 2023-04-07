038690083867273ffc791051cb64fa7d16d4a3eb
Bellagirl-maker <isabellaotoo25@gmail.com>
2023-04-07 07:00:09 +0000

    Update linters

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
 
 const TASKS = [
@@ -8,56 +8,46 @@ const TASKS = [
     index: 0,
   },
   {
-    description: 'Complete To Do Project',
+    description: 'Complete To do task',
     complete: true,
     index: 1,
   },
   {
-    description: 'Take out trash',
+    description: 'Buy Groceries',
     complete: true,
     index: 2,
   },
   {
-    description: 'Buy Groceries',
+    description: 'Take out trash',
     complete: true,
     index: 3,
   },
 ];
 
 const todoList = document.querySelector('.todo-list');
-const todoTitleBox = document.createElement('div');
+
 const refresh = document.createElement('img');
-const title = document.createElement('p');
 const input = document.createElement('input');
 const divone = document.createElement('div');
 const divtwo = document.createElement('div');
 const clear = document.createElement('a');
 const submit = document.createElement('img');
 
-title.innerText = "Today's To Do";
-title.classList.add('title-text');
 input.type = 'text';
 input.placeholder = 'Add your list...';
 input.classList.add('task-input');
 refresh.classList.add('refresh-icn');
-todoTitleBox.classList.add('title-ref-box');
-todoTitleBox.appendChild(title);
-todoTitleBox.appendChild(refresh);
 divone.classList.add('task-input-box');
 clear.innerText = 'Clear all completed';
 submit.classList.add('add-todo-btn');
 divtwo.classList.add('clear-btn-box');
 divone.appendChild(input);
 divone.appendChild(submit);
-todoList.appendChild(todoTitleBox);
 todoList.appendChild(divone);
 divtwo.appendChild(clear);
 
-
-
 const showTodo = () => {
-  let x = 0;
-  for (let x = 0; x < TASKS.length; x++); {
+  for (let i = 0; i < TASKS.length; i++) {
     const div = document.createElement('div');
     const label = document.createElement('label');
     const checkbox = document.createElement('input');
@@ -66,10 +56,10 @@ const showTodo = () => {
     menu.classList.add('list-menu-icn');
     li.classList.add('list-item');
     checkbox.type = 'checkbox';
-    checkbox.id = TASKS[x].index;
-    label.for = TASKS[x].index;
+    checkbox.id = TASKS[i].index;
+    label.for = TASKS[i].index;
 
-    label.innerText = TASKS[x].description;
+    label.innerText = TASKS[i].description;
     div.appendChild(checkbox);
     div.appendChild(label);
     li.appendChild(div);
