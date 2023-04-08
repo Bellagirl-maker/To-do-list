4c5f3f85527d234ddb42a75e63934d5a0de41eb7
Isabella Otoo <isabellaotoo25@gmail.com>
2023-04-07 23:52:23 -0700

    created the interactive list

--- src/addRemove.js
+++ src/addRemove.js
@@ -15,12 +15,40 @@ export const update = (pos, key, value) => {
   localStorage.setItem('todolist', JSON.stringify(todolist));
 };
 
-export const remove = (position) => {
-  todolist.splice(position, 1);
-
-  // update indices
+export const updateIndexes = () => {
   for (let i = 0; i < todolist.length; i += 1) {
     todolist[i].index = i + 1;
   }
+};
+
+export const remove = (position) => {
+  todolist.splice(position, 1);
+  updateIndexes();
   localStorage.setItem('todolist', JSON.stringify(todolist));
 };
+// import todolist from './todoList.js';
+
+// export const add = (task) => {
+//   todolist.push({
+//     description: task.value,
+//     completed: false,
+//     index: todolist.length + 1,
+//   });
+
+//   localStorage.setItem('todolist', JSON.stringify(todolist));
+// };
+
+// export const update = (pos, key, value) => {
+//   todolist[pos][key] = value;
+//   localStorage.setItem('todolist', JSON.stringify(todolist));
+// };
+
+// export const remove = (position) => {
+//   todolist.splice(position, 1);
+
+//   // update indices
+//   for (let i = 0; i < todolist.length; i += 1) {
+//     todolist[i].index = i + 1;
+//   }
+//   localStorage.setItem('todolist', JSON.stringify(todolist));
+// };
